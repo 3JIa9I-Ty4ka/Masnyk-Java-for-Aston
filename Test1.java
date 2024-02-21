0b05b6460f8b6ed9b518dd7cf16dcef8c402a1af
3JIa9I-Ty4ka <160742965+3JIa9I-Ty4ka@users.noreply.github.com>
2024-02-21 22:13:37 +0500

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,21 @@
+public class Test1 {
+    static public  void main(String args[]) {
+      int a = 4;
+      int b = 4;
+
+      if (a<b) {
+          System.out.println("a<b");
+      }
+      else if (a>b) {
+          System.out.println("a>b");
+      }
+      else {
+          System.out.println("a=b");
+        }
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
---
+++ Test2.java
@@ -0,0 +1,13 @@
+public class Test2 {
+    static public void main(String args[]) {
+        String a = new String("Привет");
+        String b = new String("Пока");
+
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        }
+        else {
+            System.out.println("Строки не идентичны");
+        }
+    }
+}
